abe7b1ca781e0e54ce9f2ed226c2a1f2c657a21b
Toby Keegan <Toby.Keegan@ibm.com>
2023-10-11 21:26:28 +0100

    Add persistent navbar

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,43 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+	<div class="navbar-brand">
+		<a class="navbar-item is-size-3 has-text-weight-bold" href="/"> waqq.ly </a>
+
+		<a
+			role="button"
+			class="navbar-burger"
+			aria-label="menu"
+			aria-expanded="false"
+			data-target="navbarBasicExample"
+		>
+			<span aria-hidden="true" />
+			<span aria-hidden="true" />
+			<span aria-hidden="true" />
+		</a>
+	</div>
+
+	<div id="navbarBasicExample" class="navbar-menu">
+		<div class="navbar-start">
+			<a class="navbar-item"> Home </a>
+
+			<div class="navbar-item has-dropdown is-hoverable">
+				<a class="navbar-link"> Register </a>
+
+				<div class="navbar-dropdown">
+					<a class="navbar-item" href="/register/pet"> Register a pet </a>
+					<a class="navbar-item" href="/register/walker"> Register a walker </a>
+				</div>
+			</div>
+		</div>
+
+		<div class="navbar-end">
+			<div class="navbar-item">
+				<div class="buttons">
+					<a class="button is-primary">
+						<strong>Sign up</strong>
+					</a>
+					<a class="button is-light"> Log in </a>
+				</div>
+			</div>
+		</div>
+	</div>
+</nav>
