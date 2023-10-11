3496f229415ee54315475746aa5d673358205f7c
Toby Keegan <Toby.Keegan@ibm.com>
2023-10-11 21:32:41 +0100

    update test to pass build

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('home page has welcome message', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Welcome to the waqq.ly prototype!' })).toBeVisible();
 });
