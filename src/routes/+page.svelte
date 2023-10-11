da5459bfba0a24dc8f758030dcc0164886dd3832
Toby Keegan <Toby.Keegan@ibm.com>
2023-10-11 21:26:44 +0100

    Create homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<main>
+	<div class="columns">
+		<div class="column">
+			<section class="section">
+				<h1 class="title">Welcome to the waqq.ly prototype!</h1>
+				<br />
+				<h2 class="subtitle">Waqq.ly is a service to connect 🐶 and 🚶‍♂️</h2>
+			</section>
+			<section class="section">
+				<h1 class="title">Get started 🎉</h1>
+				<br />
+				<h2 class="subtitle">
+					Start by either registering your <a class="has-text-weight-medium" href="/register/pet"
+						>pet</a
+					>, or yourself as a <a class="has-text-weight-medium" href="/register/walker">walker</a>.
+				</h2>
+			</section>
+		</div>
+		<div class="column">
+			<div class="tile is-ancestor">
+				<div class="tile is-vertical is-8">
+					<div class="tile">
+						<div class="tile is-parent is-vertical">
+							<article class="tile is-child notification is-primary">
+								<p class="title">Simple signup</p>
+								<p class="subtitle">in one simple step</p>
+							</article>
+							<article class="tile is-child notification is-warning">
+								<p class="title">Safe</p>
+								<p class="subtitle">All walkers are vetted at sign up!</p>
+							</article>
+						</div>
+						<div class="tile is-parent">
+							<article class="tile is-child notification is-info">
+								<p class="title">Walk other dogs</p>
+								<figure class="image is-4by3">
+									<img
+										src="https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+									/>
+								</figure>
+							</article>
+						</div>
+					</div>
+					<div class="tile is-parent">
+						<article class="tile is-child notification is-danger">
+							<p class="title">Free, forever!</p>
+							<p class="subtitle">We believe all pets deserve to be walked by passionate people.</p>
+							<div class="content">
+								<!-- Content -->
+							</div>
+						</article>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
