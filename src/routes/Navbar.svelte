e0291deeb60f22422ca21aa2a1b1cdd434e55a2f
Toby Keegan <Toby.Keegan@ibm.com>
2023-10-12 08:40:56 +0100

    Remove register and login buttons

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,13 +1,15 @@
-<nav class="navbar" role="navigation" aria-label="main navigation">
+<nav class="navbar" aria-label="main navigation">
 	<div class="navbar-brand">
 		<a class="navbar-item is-size-3 has-text-weight-bold" href="/"> waqq.ly </a>
 
+		<!-- svelte-ignore a11y-invalid-attribute -->
 		<a
 			role="button"
 			class="navbar-burger"
 			aria-label="menu"
 			aria-expanded="false"
 			data-target="navbarBasicExample"
+			href=""
 		>
 			<span aria-hidden="true" />
 			<span aria-hidden="true" />
@@ -17,25 +19,15 @@
 
 	<div id="navbarBasicExample" class="navbar-menu">
 		<div class="navbar-start">
-			<a class="navbar-item"> Home </a>
+			<a class="navbar-item" href="/"> Home </a>
 
 			<div class="navbar-item has-dropdown is-hoverable">
+				<!-- svelte-ignore a11y-missing-attribute -->
 				<a class="navbar-link"> Register </a>
 
 				<div class="navbar-dropdown">
 					<a class="navbar-item" href="/register/pet"> Register a pet </a>
 					<a class="navbar-item" href="/register/walker"> Register a walker </a>
-				</div>
-			</div>
-		</div>
-
-		<div class="navbar-end">
-			<div class="navbar-item">
-				<div class="buttons">
-					<a class="button is-primary">
-						<strong>Sign up</strong>
-					</a>
-					<a class="button is-light"> Log in </a>
 				</div>
 			</div>
 		</div>
