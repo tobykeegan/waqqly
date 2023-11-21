ef3f84a902f1e9aedc80f5dd1fdb1a181a2068b2
Toby Keegan <Toby.Keegan@ibm.com>
2023-11-21 20:22:18 +0000

    View db (#8)
    * Quick fix, add submit to pet form
    
    * Add dog walker form basics
    
    * New pages to view database contents
    
    * New pet walker form
    
    * Sort out pet form
    
    * Linting fixes
    
    * More code linting problems

---
+++ src/routes/register/walker/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import WalkerForm from './WalkerForm.svelte';
+</script>
+
+<WalkerForm />
--- src/routes/register/walker/WalkerForm.svelte
+++ src/routes/register/walker/WalkerForm.svelte
@@ -2,18 +2,17 @@
 	import Modal from '../Modal.svelte';
 
 	let showModal = false;
+
 	let formData = {
 		name: '',
 		email: '',
-		breed: '',
-		gender: '',
-		sterile: '',
-		info: ''
+		address: '',
+		phone: ''
 		// Add more properties as needed
 	};
 
 	function handleSubmit() {
-		const serverUrl = import.meta.env.VITE_API_URI + '/pets';
+		const serverUrl = import.meta.env.VITE_API_URI + '/users';
 
 		console.log(formData);
 
@@ -51,7 +50,7 @@
 <main>
 	<section class="hero">
 		<div class="hero-body">
-			<p class="title">Register a pet</p>
+			<p class="title">Register a dog walker</p>
 			<p class="subtitle">Please fill out this form</p>
 		</div>
 	</section>
@@ -59,33 +58,16 @@
 		<form on:submit|preventDefault={handleSubmit}>
 			<div class="container is-fluid">
 				<div class="field">
-					<label class="label" for="name">Pet Name</label>
+					<label class="label" for="name">Name</label>
 					<div class="control has-icons-left has-icons-right">
 						<input
 							bind:value={formData.name}
 							name="name"
 							class="input"
 							type="text"
-							placeholder="Pet name"
+							placeholder="John Doe"
 						/>
-						<span class="icon is-small is-left"> ✏️ </span>
-						<span class="icon is-small is-right">
-							<i class="fas fa-check" />
-						</span>
-					</div>
-				</div>
-
-				<div class="field">
-					<label class="label" for="breed">Breed</label>
-					<div class="control has-icons-left has-icons-right">
-						<input
-							bind:value={formData.breed}
-							name="breed"
-							class="input"
-							type="text"
-							placeholder="Chocolate Labrador"
-						/>
-						<span class="icon is-small is-left"> 🐶 </span>
+						<span class="icon is-small is-left"> 🧑 </span>
 						<span class="icon is-small is-right">
 							<i class="fas fa-check" />
 						</span>
@@ -100,7 +82,7 @@
 							name="email"
 							class="input"
 							type="email"
-							placeholder="thedawg@gmail.com"
+							placeholder="john@example.com"
 						/>
 						<span class="icon is-small is-left"> ✉️ </span>
 						<span class="icon is-small is-right">
@@ -110,34 +92,32 @@
 				</div>
 
 				<div class="field">
-					<label class="label" for="gender">Gender</label>
+					<label class="label" for="address">Home Address</label>
 					<div class="control">
-						<div class="select">
-							<select bind:value={formData.gender} name="gender">
-								<option value="male">Male</option>
-								<option value="female">Female</option>
-							</select>
-						</div>
-					</div>
-				</div>
-				<div class="field">
-					<div class="control">
-						<label class="checkbox">
-							<input bind:value={formData.sterile} name="sterile" type="checkbox" />
-							Spayed/neutered?
-						</label>
+						<input
+							bind:value={formData.address}
+							name="address"
+							class="input"
+							type="text"
+							placeholder="123 Main St"
+						/>
 					</div>
 				</div>
 
 				<div class="field">
-					<label class="label" for="info">Additional information</label>
-					<div class="control">
-						<textarea
-							bind:value={formData.info}
-							name="info"
-							class="textarea"
-							placeholder="Please add any details you think might be important about your pet here."
+					<label class="label" for="phone">Phone Number</label>
+					<div class="control has-icons-left has-icons-right">
+						<input
+							bind:value={formData.phone}
+							name="phone"
+							class="input"
+							type="tel"
+							placeholder="123-456-7890"
 						/>
+						<span class="icon is-small is-left"> ☎️ </span>
+						<span class="icon is-small is-right">
+							<i class="fas fa-check" />
+						</span>
 					</div>
 				</div>
 
