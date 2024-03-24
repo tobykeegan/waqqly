c949943902069326d3086c1a7e02f61cc6d7d6fd
Toby Keegan <Toby.Keegan@ibm.com>
2024-03-24 13:07:10 +0000

    Drive auth properly

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -7,9 +7,10 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "auth": {
-    "waqqly3c00b261": {
+    "waqqly": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
       "UserPoolArn": "string",
